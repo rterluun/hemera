988df47d05e93bf3001a33ba418b1f959f5ca2ae
rterluun <noreply@gmail.com>
2023-12-02 13:02:59 +0000

    slack notification

--- src/hemera/notifications.py
+++ src/hemera/notifications.py
@@ -1,2 +1,24 @@
-def hello_world():
-    print("Hello, World!")
+from logging import getLogger, Logger
+from typing import Optional
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.web.slack_response import SlackResponse
+
+
+LOGGER = getLogger(__name__)
+
+
+def send_slack_message(
+    slack_api_token: str,
+    channel: str,
+    message: str,
+    logger: Logger = LOGGER,
+) -> Optional[SlackResponse]:
+    """Send a message to a Slack channel."""
+    try:
+        client = WebClient(token=slack_api_token)
+        response = client.chat_postMessage(channel=channel, text=message)
+        return response
+    except SlackApiError as e:
+        logger.error(f"Error sending Slack message: {e.response['error']}")
+        return None
--- tests/hemera/test_notifications.py
+++ tests/hemera/test_notifications.py
@@ -1,6 +1,30 @@
-from hemera.notifications import hello_world
+from unittest.mock import patch, MagicMock
 
-def test_hello_world(capfd):
-    hello_world()
-    out, err = capfd.readouterr()
-    assert out == "Hello, World!\n"
+from hemera.notifications import send_slack_message
+
+
+@patch("slack_sdk.web.client.WebClient.api_call")
+def test_send_slack_message(
+    api_call=MagicMock(),
+    channel: str = "#channel_name",
+    message: str = "some message",
+):
+    send_slack_message(
+        slack_api_token="",
+        channel=channel,
+        message=message,
+    )
+
+    api_call.assert_called_once_with(
+        "chat.postMessage", json={"channel": channel, "text": message}
+    )
+
+
+def test_send_slack_message_error():
+    response = send_slack_message(
+        slack_api_token="invalid_token",
+        channel="#channel_name",
+        message="some message",
+    )
+
+    assert response is None
