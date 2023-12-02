a73529f54bc09cde956c68cf636dc56cbb6cf765
rterluun <noreply@gmail.com>
2023-12-02 13:16:40 +0000

    install hemera

--- github/__init__.py
+++ github/__init__.py
@@ -1,10 +1,17 @@
-import logging
 import json
+from logging import getLogger, Logger
+from os import getenv as os_getenv
 import azure.functions as func
+from hemera.notifications import send_slack_message
+
+LOGGER = getLogger(__name__)
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
+def main(
+    req: func.HttpRequest,
+    logger: Logger = LOGGER,
+) -> func.HttpResponse:
+    logger.info("Python HTTP trigger function processed a request.")
     json_data = json.dumps(
         {
             "method": req.method,
@@ -14,5 +21,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "get_body": req.get_body().decode(),
         }
     )
-    logging.info(json_data)
+    logger.info(json_data)
+
+    send_slack_message(
+        slack_api_token=os_getenv("SLACK_API_TOKEN"),
+        channel="#hemera-test",
+        message="Hello from Azure Functions!",
+    )
     return func.HttpResponse(json_data)
