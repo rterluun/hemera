120046cdc65c4ad9cf037bb2b9b9b781054bd9a9
rterluun <noreply@gmail.com>
2023-12-02 13:01:43 +0000

    after pre-commit

--- github/__init__.py
+++ github/__init__.py
@@ -2,14 +2,17 @@ import logging
 import json
 import azure.functions as func
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    json_data = json.dumps({
-        'method': req.method,
-        'url': req.url,
-        'headers': dict(req.headers),
-        'params': dict(req.params),
-        'get_body': req.get_body().decode()
-    })
+    logging.info("Python HTTP trigger function processed a request.")
+    json_data = json.dumps(
+        {
+            "method": req.method,
+            "url": req.url,
+            "headers": dict(req.headers),
+            "params": dict(req.params),
+            "get_body": req.get_body().decode(),
+        }
+    )
     logging.info(json_data)
     return func.HttpResponse(json_data)
