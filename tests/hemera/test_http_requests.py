4ba9238d8f88cb3feea6bc4148019f62017eec93
rterluun <noreply@gmail.com>
2023-12-04 14:55:24 +0000

    Move header and body dict to a resource file, keep fixtures readable #11

--- tests/hemera/test_http_requests.py
+++ tests/hemera/test_http_requests.py
@@ -5,29 +5,19 @@ from hemera.http_request_handler import (
     convert_http_request_headers_to_dict,
     convert_http_request_to_dict,
 )
+from tests.hemera.resources.http_request_data import BODY, HEADER
 
 
 def test_get_http_request_headers(test_request: func.HttpRequest) -> None:
-    assert convert_http_request_headers_to_dict(req=test_request) == {
-        "x-forwarded-proto": "http",
-        "content-type": "application/json",
-    }
+    assert convert_http_request_headers_to_dict(req=test_request) == HEADER
 
 
 def test_convert_http_request_body_to_dict(test_request: func.HttpRequest) -> None:
-    assert convert_http_request_body_to_dict(req=test_request) == {
-        "action": "opened",
-        "number": 1,
-        "pull_request": {"url": "http://fakeurl.com", "id": 123456789},
-    }
+    assert convert_http_request_body_to_dict(req=test_request) == BODY
 
 
 def test_convert_http_request_to_dict(test_request: func.HttpRequest) -> None:
     assert convert_http_request_to_dict(req=test_request) == {
-        "header": {"x-forwarded-proto": "http", "content-type": "application/json"},
-        "body": {
-            "action": "opened",
-            "number": 1,
-            "pull_request": {"url": "http://fakeurl.com", "id": 123456789},
-        },
+        "header": HEADER,
+        "body": BODY,
     }
